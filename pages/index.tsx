f613379203a1438dba77c9f012ede5ee1ab00808
Anthony Washington <awashington251@gmail.com>
2021-07-10 19:23:14 -0400

    pages: Add favicon tag elements for variants

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,27 @@ export default function Home() {
           name="description"
           content="Anthony Washington Senior Software Engineer personal website"
         />
-        <link rel="icon" href="/favicon.png" />
+        <meta name="msapplication-TileColor" content="#191923" />
+        <meta name="theme-color" content="#191923" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#191923" />
       </Head>
       <main>Welcome to Anthony&apos;s personal website!</main>
     </>
