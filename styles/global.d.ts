4c83d84e51cb07e03735cfb3cddf92d01bcba9b2
Anthony Washington <awashington251@gmail.com>
2021-07-12 09:27:44 -0400

    styles: define global default theme type for react-jss
    github.com/cssinjs/jss/blob/master/docs/react-jss-ts.md

---
+++ styles/global.d.ts
@@ -0,0 +1,14 @@
+declare global {
+  namespace Jss {
+    export interface Theme {
+      palette: {
+        primary: string
+        secondary: string
+        grey: string
+        white: string
+      }
+    }
+  }
+}
+
+export {}
