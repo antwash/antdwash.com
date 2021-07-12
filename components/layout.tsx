e6e4d35cc1a45e7164f0fc54a539a1c0d1b6e7a6
Anthony Washington <awashington251@gmail.com>
2021-07-12 08:44:16 -0400

    layout: encapsulate social media side bar component

--- components/layout.tsx
+++ components/layout.tsx
@@ -2,27 +2,26 @@ import React from "react"
 
 import { createUseStyles } from "react-jss"
 
+import { SocialMediaBar } from "@components/socialMediaBar"
+
 const useStyles = createUseStyles({
   container: {
-    backgroundColor: "orange",
+    backgroundColor: "#191923",
     minHeight: "100vh",
     display: "grid",
     gridTemplateColumns: "0.1fr 1fr",
     gridTemplateRows: "0.1fr 1fr 0.1fr",
     gridTemplateAreas: `
       'nav nav'
-      'sidebar content'
-      'footer footer'
+      'socialMedia content'
+      'socialMedia footer'
     `,
   },
   nav: {
     backgroundColor: "purple",
     gridArea: "nav",
   },
-  sideBar: {
-    backgroundColor: "lightblue",
-    gridArea: "sidebar",
-  },
+  socialMedia: { gridArea: "socialMedia" },
   content: {
     backgroundColor: "red",
     gridArea: "content",
@@ -38,20 +37,20 @@ const useStyles = createUseStyles({
       gridTemplateAreas: `
         'nav'
         'content'
-        'sidebar'
+        'socialMedia'
         'footer'
       `,
     },
   },
 })
 
-export const Layout = () => {
+const Layout = () => {
   const classes = useStyles()
 
   return (
     <div className={classes.container}>
       <nav className={classes.nav}>NavBar</nav>
-      <div className={classes.sideBar}>Social Media side bar</div>
+      <SocialMediaBar className={classes.socialMedia} />
       <main className={classes.content}>
         <span>Welcome to Anthony&apos;s personal website!</span>
       </main>
@@ -59,3 +58,4 @@ export const Layout = () => {
     </div>
   )
 }
+export default Layout
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from "next/head"
 
-import { Layout } from "@components"
+import Layout from "@components/layout"
 
 export default function Home() {
   return (
