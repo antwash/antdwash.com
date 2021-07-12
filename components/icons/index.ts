338f2e40c3a3e429d7996614d803c91961d8ae57
Anthony Washington <awashington251@gmail.com>
2021-07-12 08:40:34 -0400

    components: create icon

---
+++ components/icons/index.ts
@@ -0,0 +1,9 @@
+import Github from "./github"
+import Instagram from "./instagram"
+import Linkedin from "./linkedin"
+import Twitter from "./twitter"
+import Youtube from "./youtube"
+import { Icon, IconType } from "./icon"
+
+export { Icon, Github, Youtube, Twitter, Instagram, Linkedin }
+export type { IconType }
