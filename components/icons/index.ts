932fe8ce39082b7ffbb16bf9980279e9ff186a2d
Anthony Washington <awashington251@gmail.com>
2021-07-12 21:58:05 -0400

    icons: Switch support logo icon

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -1,9 +1,10 @@
 import Github from "./github"
 import Instagram from "./instagram"
 import Linkedin from "./linkedin"
+import Logo from "./logo"
 import Twitter from "./twitter"
 import Youtube from "./youtube"
 import { Icon, IconType } from "./icon"
 
-export { Icon, Github, Youtube, Twitter, Instagram, Linkedin }
+export { Icon, Github, Youtube, Twitter, Instagram, Linkedin, Logo }
 export type { IconType }
