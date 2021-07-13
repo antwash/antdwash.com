4b8ce6198bd49234fc8e53b8d641b631d0f7f43d
Anthony Washington <awashington251@gmail.com>
2021-07-12 21:57:28 -0400

    icons: Add logo

---
+++ components/icons/logo.tsx
@@ -0,0 +1,22 @@
+const Logo = () => {
+  return (
+    <svg viewBox="0 0 150 150" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect
+        x="4.94974"
+        y="75.4769"
+        width="99.0661"
+        height="99.0661"
+        rx="16.5"
+        transform="rotate(-44.9166 4.94974 75.4769)"
+        stroke="#C81D25"
+        strokeWidth="7"
+      />
+      <path
+        d="M84.676 82.3194H65.412L60.292 96.0154H55.812L72.836 51.2154H77.124L94.34 96.0154H89.86L84.676 82.3194ZM83.332 78.6714L75.076 56.9114H74.82L66.756 78.6714H83.332Z"
+        fill="#FBFEF9"
+      />
+    </svg>
+  )
+}
+
+export default Logo
