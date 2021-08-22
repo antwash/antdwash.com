d86ca804deaebad3bf1109947d22f27618cdcadc
Anthony Washington <awashington251@gmail.com>
2021-08-21 21:24:20 -0400

    section home: Add fade down animation to text

--- components/sections/home.tsx
+++ components/sections/home.tsx
@@ -1,6 +1,8 @@
 import React from "react"
 
-import { createUseStyles } from "react-jss"
+import { createUseStyles, useTheme } from "react-jss"
+
+import { CSSTransition } from "react-transition-group"
 
 import { Section } from "@sections"
 import { Hyperlink, Text, Button } from "@components"
@@ -20,48 +22,91 @@ export const Home = () => {
       marginTop: theme.spacing.lg,
       gap: theme.spacing.lg,
     },
+    appear: {
+      ...theme.animation.fadeDownAppear,
+    },
+    appearActive: {
+      ...theme.animation.fadeDownAppearActive,
+    },
   }))()
+
+  const hello = (
+    <Text
+      variant="h4"
+      color="secondary"
+      family="roboto"
+      className={classes.text}
+    >
+      Hello, I am
+    </Text>
+  )
+
+  const name = (
+    <Text variant="h1" color="primary" className={classes.text}>
+      Anthony Washington.
+    </Text>
+  )
+
+  const title = (
+    <Text variant="h1" color="grey" className={classes.text}>
+      Senior Software Engineer.
+    </Text>
+  )
+
+  const responsibility = (
+    <Text variant="p" color="grey" family="roboto" className={classes.mission}>
+      I prioritize designing and building intuitive digital experiences with an
+      emphasizes on UI/UX. Currently, I’m building an IOT, cloud connected
+      robotic makeline to automate food preparation
+      <Hyperlink url="https://usehyphen.com/" title=" @Hyphen" />.
+    </Text>
+  )
+
+  const actions = (
+    <div className={classes.actions}>
+      <Button
+        label="Email Me"
+        onClick={() => {
+          window.open("mailto:awashington251@gmail.com", "_self")
+        }}
+      />
+      <Button
+        label="Get Resume"
+        onClick={() => {
+          window.open("", "_blank")
+        }}
+      />
+    </div>
+  )
+
+  const elements: React.ReactNode[] = [
+    hello,
+    name,
+    title,
+    responsibility,
+    actions,
+  ]
+
+  const { animation }: Jss.Theme = useTheme()
+
   return (
     <Section>
-      <Text
-        variant="h4"
-        color="secondary"
-        family="roboto"
-        className={classes.text}
-      >
-        Hello, I am
-      </Text>
-      <Text variant="h1" color="primary" className={classes.text}>
-        Anthony Washington.
-      </Text>
-      <Text variant="h1" color="grey" className={classes.text}>
-        Senior Software Engineer.
-      </Text>
-      <Text
-        variant="p"
-        color="grey"
-        family="roboto"
-        className={classes.mission}
-      >
-        I prioritize designing and building intuitive digital experiences with
-        an emphasizes on UI/UX. Currently, I’m building an IOT, cloud connected
-        robotic makeline to automate food preparation
-        <Hyperlink url="https://usehyphen.com/" title=" @Hyphen" />.
-      </Text>
-      <div className={classes.actions}>
-        <Button
-          label="Email Me"
-          onClick={() => {
-            window.open("mailto:awashington251@gmail.com", "_self")
-          }}
-        />
-        <Button
-          label="Get Resume"
-          onClick={() => {
-            window.open("", "_blank")
-          }}
-        />
-      </div>
+      {elements.map((element, index) => {
+        return (
+          <CSSTransition
+            in
+            appear
+            key={index}
+            timeout={animation.delay}
+            classNames={{
+              appear: classes.appear,
+              appearActive: classes.appearActive,
+            }}
+          >
+            <div style={{ transitionDelay: `0.${index * 2}s` }}>{element}</div>
+          </CSSTransition>
+        )
+      })}
     </Section>
   )
 }
