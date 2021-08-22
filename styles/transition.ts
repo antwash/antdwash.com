628171772b2e11fd978618a0995d032c48f75898
Anthony Washington <awashington251@gmail.com>
2021-08-21 21:41:48 -0400

    styles: Create animation transition hook

---
+++ styles/transition.ts
@@ -0,0 +1,26 @@
+import { createUseStyles } from "react-jss"
+
+const delay = 200
+const useTransitionStyles = createUseStyles(() => ({
+  fadeAppear: {
+    opacity: 0,
+  },
+  fadeAppearActive: {
+    opacity: 1,
+    transition: "opacity 0.6s cubic-bezier(0.7, 0, 0.84, 0)",
+  },
+  fadeDownAppear: {
+    opacity: 0,
+    transform: "translateY(-20px)",
+    transition:
+      "opacity 0.6s cubic-bezier(0.7, 0, 0.84, 0), transform 0.2s cubic-bezier(0.7, 0, 0.84, 0)",
+  },
+  fadeDownAppearActive: {
+    opacity: 1,
+    transform: "translateY(0)",
+    transition:
+      "opacity 0.6s cubic-bezier(0.7, 0, 0.84, 0), transform 0.2s cubic-bezier(0.7, 0, 0.84, 0)",
+  },
+}))
+
+export { useTransitionStyles, delay }
