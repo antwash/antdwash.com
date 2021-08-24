6b801dab6c14413dc8b15d6c2cc6ea807c89569a
Anthony Washington <awashington251@gmail.com>
2021-08-24 08:48:35 -0400

    utils: define scroll reveal config options

---
+++ utils/index.ts
@@ -0,0 +1,9 @@
+const scrollRevealConfig: scrollReveal.ScrollRevealObjectOptions = {
+  distance: "3rem",
+  duration: 600,
+  delay: 200,
+  viewFactor: 0.25,
+  easing: "cubic-bezier(0.7, 0, 0.84, 0)",
+}
+
+export { scrollRevealConfig }
