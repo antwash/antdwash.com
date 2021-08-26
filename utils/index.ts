08ff016e97a3e6dd0e995c4b3732eee655f591c5
Anthony Washington <awashington251@gmail.com>
2021-08-25 20:16:55 -0400

    adjust scroll reveal config

--- utils/index.ts
+++ utils/index.ts
@@ -1,7 +1,8 @@
 const scrollRevealConfig: scrollReveal.ScrollRevealObjectOptions = {
+  origin: "top",
   distance: "3rem",
-  duration: 600,
-  delay: 200,
+  duration: 300,
+  delay: 100,
   viewFactor: 0.25,
   easing: "cubic-bezier(0.7, 0, 0.84, 0)",
 }
